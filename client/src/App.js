7ec7ff64769ca1e98e56be84f07e45ba800c8f47
Marissa <marissacooterl@gmail.com>
2020-03-23 16:19:19 -0500

    Finished part two. just adding functionality to home button

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <Route path="/" component = {MovieList}/>
+      <Route exact path="/" component = {MovieList}/>
       <Route path="/movies/:movieID" component = {Movie}/>
     </div>
   );
